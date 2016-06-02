492651a0a22cd7f7bea51e8fb71940736c64f221
Chrisbooty <1163209271@qq.com>
2016-06-02 20:04:02 +0800

    Initial Commit

---
+++ CJMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CJMap
+//
+//  Created by mac on 16/6/2.
+//  Copyright © 2016年 Cijian.Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CJMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CJMap
+//
+//  Created by mac on 16/6/2.
+//  Copyright © 2016年 Cijian.Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
